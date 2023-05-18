4a9b2f47949b54b6b3ed924c666cc3199e729950
Horace <minhhieudn98@gmail.com>
2023-05-18 21:00:46 +0700

    Add FE libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={''} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <h1>
+          Welcome to dev team nha lam
+        </h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
